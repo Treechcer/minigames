680673262d657501ebd85db12a53ae93526068eb
Treechcer <150960915+Treechcer@users.noreply.github.com>
2025-04-01 08:16:31 +0000

    implemented basic dice roll, will do the landing pade and dice roll website visuals next later :)

--- dice roll/script.js
+++ dice roll/script.js
@@ -1,1 +1,7 @@
-//future code goes here rah
+function rollTheDice(){
+    return Math.ceil(Math.random() * 6)
+}
+
+function gameLoop(){
+    document.getElementById("text").textContent = rollTheDice()
+}
