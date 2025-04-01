c6a095b65dafb3be7cc339b894a9238dc3a203b4
Treecher <martin.vrobel308@gmail.com>
2025-04-01 19:47:07 +0200

    I've made the dice roller better, still looks bad but functions more like convetional dice

--- dice roll/script.js
+++ dice roll/script.js
@@ -3,5 +3,54 @@ function rollTheDice(){
 }
 
 function gameLoop(){
-    document.getElementById("text").textContent = rollTheDice()
+    var num = rollTheDice()
+    num = 6
+    if (num == 1){
+        for (let i = 1; i < 10; i++){
+            if (i == 5){
+                continue
+            }
+            document.getElementById(i).style.visibility = "hidden"
+        }
+    }
+    if (num == 2){
+        for (let i = 1; i < 10; i++){
+            if (i == 1 || i == 9){
+                continue
+            }
+            document.getElementById(i).style.visibility = "hidden"
+        }
+    }
+    if (num == 3){
+        for (let i = 1; i < 10; i++){
+            if (i == 3 || i == 5 || i == 7){
+                continue
+            }
+            document.getElementById(i).style.visibility = "hidden"
+        }
+    }
+    if (num == 4){
+        for (let i = 1; i < 10; i++){
+            if (i == 1 || i == 3 || i == 7 ||i == 9){
+                continue
+            }
+            document.getElementById(i).style.visibility = "hidden"
+        }
+    }
+    if (num == 5){
+        for (let i = 1; i < 10; i++){
+            if (i == 1 || i == 3 || i == 7 ||i == 9 || i == 5){
+                continue
+            }
+            document.getElementById(i).style.visibility = "hidden"
+        }
+    }
+    if (num == 6){
+        for (let i = 1; i < 10; i++){
+            if (i == 1 || i == 3 || i == 7 ||i == 9 || i == 4 || i == 6){
+                continue
+            }
+            document.getElementById(i).style.visibility = "hidden"
+        }
+    }
 }
